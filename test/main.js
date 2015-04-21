4f1fc9ebc9c2536f7a8aed49e4cac3e5c7bf3931
Viacheslav Bereza <betula.icons8@gmail.com>
2015-04-21 09:54:59 +0300

    Add svg test

--- test/main.js
+++ test/main.js
@@ -8,6 +8,16 @@ describe('impress-html-angular-minify', function () {
     var
       minify = new HtmlAngularMinify();
 
-    should(minify.apply('<input ng-model="model" class="ng-scope" required="required" />')).equal('<input class="" required="required"/>');
+    should(minify.apply('<input ng-model="model" class="ng-scope" required="required" />'))
+      .equal('<input class="" required="required"/>');
   });
+
+  //it('svg should work', function() {
+  //  var
+  //    minify = new HtmlAngularMinify();
+  //
+  //  should(minify.apply('<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26"><g></g></svg>'))
+  //    .equal('<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26"><g></g></svg>');
+  //});
+
 });
